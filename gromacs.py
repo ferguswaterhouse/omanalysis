5452e5c7333da590b993f1d8b90c3a6964b4f9cf
ferguswaterhouse <fergus.waterhouse@merton.ax.ac.uk>
2023-02-13 14:34:39 +0000

    Density functionality.

--- gromacs.py
+++ gromacs.py
@@ -1,11 +1,10 @@
 import subprocess
 
-def density(xtc_file, tpr_file, ndx_file, out_file, ndx_group_name):
+def density(xtc_file, tpr_file, ndx_file, out_file, ndx_group_name, density_input):
     # Runs gmx density for the given index group along the Z axis taking 100 slices
     density_command = "gmx density -f {xtc} -s {tpr} -n {ndx} -d Z -o {out} -center -sl 100 -dens number".format(
         xtc=xtc_file,
         tpr=tpr_file,
         ndx=ndx_file,
         out=out_file)
-    density_input = 'ALL_TERM\n{gnm}\n'.format(gnm=ndx_group_name)
     subprocess.run(density_command, shell=True, input=density_input, encoding='ascii')
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import argparse
+import gromacs as gmx
 
 
 if __name__ == '__main__':
+    
+    # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-txt')
+
+    parser.add_argument('command', help='Command to run. Options: [complete, msd, density]')
+
+    gmx_files = ['gro', 'xtc', 'tpr', 'ndx', 'edr']
+    for file_type in gmx_files:
+        parser.add_argument(f'-{file_type}', type=str)
+
+    special_arguments = ['out', 'deffnm', 'gnm']
+    for arg in special_arguments:
+        parser.add_argument(f'-{arg}', type=str)
+
     args = parser.parse_args()
-    print(args.txt)
+
+    # Run the command
+    if args.command == 'density':
+        gmx.density(args.xtc, args.tpr, args.ndx, args.out, args.gnm, density_input='ALL_TERM\n{gnm}\n'.format(gnm=args.gnm))
+    else:
+        print('Command not recognized')
